34f8ffbc8f20202d47627c27b5ff9e5f3d585883
Tbryan4 <tbryan4@nait.ca>
2024-03-31 11:31:00 -0600

    Project Start

---
+++ Models/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project1.Models
+{
+    public partial class Blog
+    {
+        public int BlogId { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public string? Author { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
