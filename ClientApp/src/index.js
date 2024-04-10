871ee59b61ee0db411dc54b2eb5650c4d68fe6bd
Tbryan4 <tbryan4@nait.ca>
2024-04-09 20:44:39 -0600

    I got the navbar to appear properly

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,10 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import './styles.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Navbar2} from "./Components/Navbar/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
+    <Navbar2></Navbar2>,
     <App></App>
 )
