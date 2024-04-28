4cf55ecd6dbb6c1e26ab03bfd4f1d3ab3a3dd539
Tbryan4 <tbryan4@nait.ca>
2024-04-27 19:42:53 -0600

    added first comopnent for program collection

--- ClientApp/src/Pages/home.js
+++ ClientApp/src/Pages/home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { CarouselDefault } from '../Components/Carousel/Carousel';
+import {ProgramCollection} from "../Components/ProgramCollection/ProgramCollection";
 
 
 
@@ -11,6 +12,7 @@ const Home = () => {
                              buttons={[{ title: "Register" }]}
                              image={{ src: "https://skatecanada.ca/wp-content/uploads/2019/12/CanPower-Cropped.jpg", alt: "Image Alt Text" }}>
             </CarouselDefault>
+            <ProgramCollection/>
         </div>
     );
 };
