c6fcfc6b104e9fcea2ba37371476ec82c55312c0
Tylen <tbryan4@nait.ca>
2024-05-02 23:21:33 -0600

    gghgg

--- ClientApp/src/Pages/home.js
+++ ClientApp/src/Pages/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CarouselDefault } from '../Components/Carousel/Carousel';
 import {ProgramCollection} from "../Components/ProgramCollection/ProgramCollection";
+import logo from "../assets/main-website-logo.png";
 
 
 
@@ -12,6 +13,11 @@ const Home = () => {
                              buttons={[{ title: "Register" }]}
                              image={{ src: "https://skatecanada.ca/wp-content/uploads/2019/12/CanPower-Cropped.jpg", alt: "Image Alt Text" }}>
             </CarouselDefault>
+            <div className={'home-title-container'}>
+                <img src={logo} width={60} alt="Main Website Logo"/>
+                <h1>Welcome to Federation Skating Club</h1>
+                <h2>Providing quality skating lessons for all ages and skill levels since 1947! Check out our PreCanSkate, CanSkate, Figure Skating, CanPower, and Adult Skating classes </h2>
+            </div>
             <ProgramCollection/>
         </div>
     );
