98db05528e74e1f65b3194377f7950a3c52d693a
Tbryan4 <tbryan4@nait.ca>
2024-09-28 23:37:48 -0600

    bug fixes

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import {Route, Switch, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
 import { ThemeProvider } from '@material-tailwind/react';
 import { GlobalNavbar } from './Components/Navbar/Navbar';
 import { FooterWithSocialLinks } from './Components/Footer/Footer';
@@ -26,47 +26,52 @@ import LearnToSkateInformationPage from "./Pages/LearnToSkateParentInformation";
 
 import ReactGA from 'react-ga4';
 
-// Initialize Google Analytics once
 ReactGA.initialize('G-JRWRLDDGCR');
 
 const App = () => {
-    // Custom hook to track page views on route changes
+    return (
+        <ThemeProvider>
+            <Router>
+                <TrackPageViews />
+                <GlobalNavbar />
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/aboutus" component={AboutPage} />
+                    <Route path="/coach" component={Team2Props} />
+                    <Route path="/contactus" component={ContactUsPage} />
+                    <Route path="/executives" component={ExecutiveTeam}/>
+                    <Route path="/events" component={EventsPage}/>
+                    <Route path="/blogs" component={BlogsPage}/>
+                    <Route path="/schedule" component={SchedulePage}/>
+                    {/* Programs */}
+                    <Route path="/adultcanskate" component={AdultCanSkatePage}/>
+                    <Route path="/canpower" component={CanPowerSkatePage}/>
+                    <Route path="/canskate" component={CanSkatePage}/>
+                    <Route path="/precanskate" component={PreCanSkatePage}/>
+                    <Route path="/introtofigureskating" component={IntroToFigureSkatingPage}/>
+                    <Route path="/adultfigureskating" component={AdultFigureSkatingPage}/>
+                    <Route path="/starskate" component={StarSkatePage}/>
+                    <Route path="/competitiveskate" component={CompetitiveSkatePage}/>
+                    <Route path="/dropin" component={DropInPage}/>
+                    <Route path="/recommendedvendors" component={RecommendedVendorsPage}/>
+                    <Route path="/fsparentinformation" component={FigureSkateParentInformationPage}/>
+                    <Route path="/ltsparentinformation" component={LearnToSkateInformationPage}/>
+                </Switch>
+                <FooterWithSocialLinks />
+            </Router>
+        </ThemeProvider>
+    );
+};
+
+// New component to handle tracking page views
+const TrackPageViews = () => {
     const location = useLocation();
 
     useEffect(() => {
-        // Send pageview hit to Google Analytics on route change
         ReactGA.send({ hitType: 'pageview', page: location.pathname });
     }, [location]);
 
-    return (
-        <ThemeProvider>
-            <GlobalNavbar />
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route path="/aboutus" component={AboutPage} />
-                <Route path="/coach" component={Team2Props} />
-                <Route path="/contactus" component={ContactUsPage} />
-                <Route path="/executives" component={ExecutiveTeam} />
-                <Route path="/events" component={EventsPage} />
-                <Route path="/blogs" component={BlogsPage} />
-                <Route path="/Schedule" component={SchedulePage} />
-                {/* Programs */}
-                <Route path="/adultcanskate" component={AdultCanSkatePage} />
-                <Route path="/canpower" component={CanPowerSkatePage} />
-                <Route path="/canskate" component={CanSkatePage} />
-                <Route path="/precanskate" component={PreCanSkatePage} />
-                <Route path="/introtofigureskating" component={IntroToFigureSkatingPage} />
-                <Route path="/adultfigureskating" component={AdultFigureSkatingPage} />
-                <Route path="/starskate" component={StarSkatePage} />
-                <Route path="/competitiveskate" component={CompetitiveSkatePage} />
-                <Route path="/dropin" component={DropInPage} />
-                <Route path="/recommendedvendors" component={RecommendedVendorsPage} />
-                <Route path="/fsparentinformation" component={FigureSkateParentInformationPage} />
-                <Route path="/ltsparentinformation" component={LearnToSkateInformationPage} />
-            </Switch>
-            <FooterWithSocialLinks />
-        </ThemeProvider>
-    );
+    return null; // This component doesn't render anything
 };
 
 export default App;
